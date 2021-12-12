8300f80dc0985fe3381eeb45e5cf036125d508b0
j_rezanina <jan.rezanina02@seznam.cz>
2021-12-12 03:32:51 +0100

    pridal jsem tridu trojuhelnik a zadefinoval body

--- Trojuhelnik/Program.cs
+++ Trojuhelnik/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Drawing;
 
 namespace Trojuhelnik
 {
+    class Trojuhelnik
+    {
+        private Point a;
+        private Point b;
+        private Point c;
+        public Trojuhelnik(Point a, Point b, Point c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Point a = new Point(3, 4);
+            Point b = new Point(5, 6);
+            Point c = new Point(7, 8);
+            Trojuhelnik t = new Trojuhelnik(a, b, c);
         }
     }
 }
