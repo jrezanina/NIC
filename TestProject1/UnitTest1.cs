991cbc75de5dd53926ed2007affab568494b6bbd
Fenriir <k3krizanova@gmail.com>
2021-12-15 18:20:26 +0100

    Přidala jsem testování metody DelkaStran

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+using System.Drawing;
+using Trojuhelnik;
+
+namespace TestProject1
+{
+    public class TrojuhelnikTest
+    {
+        [Test]
+        public void DelkaStran_pro_spravny_parametr_vraci_spravny_vysledek()
+        {
+            Point a = new Point(1, 2);
+            Point b = new Point(1, 4);
+            Point c = new Point(3, 4);
+            Trojuhelnik.Trojuhelnik t = new Trojuhelnik.Trojuhelnik(a, b, c);
+
+            double delka = t.DelkaStrany('a');
+
+            Assert.AreEqual(2, delka);
+        }
+        public void DelkaStran_pro_spatny_parametr_vraci_nulu()
+        {
+            Point a = new Point(1, 2);
+            Point b = new Point(1, 4);
+            Point c = new Point(3, 4);
+            Trojuhelnik.Trojuhelnik t = new Trojuhelnik.Trojuhelnik(a, b, c);
+
+            double delka = t.DelkaStrany('d');
+
+            Assert.AreEqual(0, delka);
+        }
+    }
+}
