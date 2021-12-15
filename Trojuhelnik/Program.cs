e58248f9f94fbde07c776b9c3f269a824f044bf2
j_rezanina <jan.rezanina02@seznam.cz>
2021-12-15 15:21:23 +0100

    Pridal jsem kod metody DelkaStrany

--- Trojuhelnik/Program.cs
+++ Trojuhelnik/Program.cs
@@ -14,6 +14,16 @@ namespace Trojuhelnik
             this.b = b;
             this.c = c;
         }
+        public double DelkaStrany(char strana)
+        {
+            if (strana == 'a')
+                return Math.Sqrt((Math.Pow(b.X - c.X, 2) + Math.Pow(b.Y - c.Y, 2)));
+            else if (strana == 'b')
+                return Math.Sqrt((Math.Pow(c.X - a.X, 2) + Math.Pow(c.Y - a.Y, 2)));
+            else if (strana == 'c')
+                return Math.Sqrt((Math.Pow(b.X - a.X, 2) + Math.Pow(b.Y - a.Y, 2)));
+            return 0;
+        }
     }
     class Program
     {
@@ -23,6 +33,11 @@ namespace Trojuhelnik
             Point b = new Point(5, 6);
             Point c = new Point(7, 8);
             Trojuhelnik t = new Trojuhelnik(a, b, c);
+
+            Console.WriteLine("Trojúhleník");
+            Console.WriteLine($"Délka strany a: {t.DelkaStrany('a')}");
+            Console.WriteLine($"Délka strany b: {t.DelkaStrany('b')}");
+            Console.WriteLine($"Délka strany c: {t.DelkaStrany('c')}");
         }
     }
 }
